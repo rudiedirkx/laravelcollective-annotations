2f7346ebfa31de53aed1623fec87de5da86fae7c
Rudie Dirkx <github@hotblocks.nl>
2025-01-05 15:26:01 +0100

    +AnnotationsToAttributesRector

--- readme.md
+++ readme.md
@@ -1,8 +1,86 @@
 **This is a `laravelcollective/annotations` drop-in replacement**
 
-To use this package instead of `laravelcollective/annotations` as a perfect drop-in replacement, do this in your project:
+To use this package instead of `laravelcollective/annotations` as a perfect drop-in replacement, do
+this in your project:
 
 1. `composer require rdx/laravelcollective-annotations`
 2. if your project explicitly required `annotations`: `composer remove laravelcollective/annotations`
 
-This will install `rdx/laravelcollective-annotations` and pretend it IS `laravelcollective/annotations`, and all other packages will believe `laravelcollective/annotations` is installed, because Composer is awesome.
+This will install `rdx/laravelcollective-annotations` and pretend it IS `laravelcollective/annotations`,
+and all other packages will believe `laravelcollective/annotations` is installed, because Composer is awesome.
+
+## UPGRADE to 9.0 (PHP Attributes instead of `doctrine/annotations`)
+
+1. Download and set up `rector/rector`, see config below.
+2. Add rule `Collective\Annotations\Rector\AnnotationsToAttributesRector` to Rector set.
+3. Run Rector **with debug enabled**, see command below.
+4. Manually fix skipped conversions due to complicated annotation syntax, see complicated example below.
+5. Run your cs fixer to import all FQCN, see `php-cs-fixer` rule below.
+6. Set `$useAttribute` to `true` in your `AnnotationsServiceProvider`, run `route:scan`, and see if all
+    that worked.
+
+### Example Rector config
+
+```php
+return RectorConfig::configure()
+	->withPaths([
+		__DIR__ . '/app/Http/Controllers',
+	])
+	->withRules([
+		AnnotationsToAttributesRector::class,
+	])
+;
+```
+
+### Run Rector command
+
+```bash
+vendor/bin/rector process -v --debug --no-diffs app/Http/
+```
+
+This will print all analyzed files, and potential errors, and might include `WARNING` lines for
+skipped methods, like:
+
+```
+  WARNING: getPhaseDownload contains unconverted {} properties
+```
+
+### Too complicated annotation
+
+```
+@Get("/some/{thing}/download.{ext}", as="thing.download", where={"ext"="json|pdf|xlsx"})
+```
+
+The path isn't a problem, nor the `as=`, but the `where=` contains another inner structure, and that
+won't be converted correctly, so they're broken now, so you have to fix those right after.
+
+### php-cs-fixer rule
+
+I assume you have `php-cs-fixer` set up. Add this rule **temporarily**:
+
+```php
+[
+	'fully_qualified_strict_types' => [
+		'import_symbols' => true,
+		'phpdoc_tags' => [],
+	],
+]
+```
+
+and run `fix` **only for the Controllers dir**:
+
+```bash
+vendor/bin/php-cs-fixer fix app/Http/Controllers/
+```
+
+This will convert all the Rector added FQCN to imports, but only those, only in the `Controllers` dir.
+
+If `php-cs-fixer` skips files and complains about "errors reported during linting", the Rector rule
+isn't good enough, and something is broken:
+
+```
+Files that were not fixed due to errors reported during linting before fixing:
+   1) /var/www/myproject/app/Http/Controllers/MyController.php
+```
+
+You'll have to fix those manually.
---
+++ src/Rector/AnnotationsToAttributesRector.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Collective\Annotations\Rector;
+
+use Illuminate\Database\Eloquent\Model;
+use PhpParser\Comment\Doc;
+use PhpParser\Node;
+use PhpParser\Node\Attribute;
+use PhpParser\Node\AttributeGroup;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PHPStan\Analyser\Scope;
+use Rector\Rector\AbstractRector;
+
+class AnnotationsToAttributesRector extends AbstractRector {
+
+	private const ROUTE_NAMESPACE = 'Collective\Annotations\Routing\Attributes\Attributes';
+	private const MODEL_NAMESPACE = 'Collective\Annotations\Database\Eloquent\Attributes\Attributes';
+	private const MODEL_ANNOTATIONS = ['Bind'];
+
+	public function getNodeTypes(): array
+	{
+		return [Class_::class, ClassMethod::class];
+	}
+
+	/**
+	 * @param Class_|ClassMethod $node
+	 * @return null|Class_|ClassMethod
+	 */
+	public function refactor(Node $node)
+	{
+// echo $node->name->name, "\n";
+		// Ignore Model methods
+		if ($node instanceof ClassMethod) {
+			$scope = $node->getAttribute('scope');
+			if ($scope instanceof Scope) {
+				$parent = $scope->getClassReflection();
+				if ($parent && $parent->isSubclassOf(Model::class)) {
+					return null;
+				}
+			}
+		}
+
+		$doc = $node->getDocComment();
+		if (!$doc) {
+			return null;
+		}
+		$phpdoc = $doc->getText();
+
+		$lines = explode("\n", $phpdoc);
+		$newComment = [];
+		foreach ($lines as $line) {
+			if (preg_match('#@(?:Any|Controller|Delete|Get|Middleware|Options|Patch|Post|Put|Resource|Route|Where|Bind)\(.+#', $line, $match)) {
+				if ($this->addAnnotation($node, $match[0])) {
+					continue;
+				}
+			}
+
+			$newComment[] = $line;
+		}
+
+		// Nothing changed, so really change nothing
+		if (count($newComment) == count($lines)) {
+			return null;
+		}
+
+		// Only /** and */ are left, so remove the whole thing
+		if (count($newComment) == 2) {
+			// array_splice($newComment, 1, 0, str_replace(' */', ' *', $newComment[1]));
+			// $newComment = [];
+			$comments = array_values(array_filter($node->getComments(), function($comment) {
+				return !($comment instanceof Doc);
+			}));
+			$node->setAttribute('comments', $comments);
+		}
+		else {
+			$node->setDocComment(new Doc(implode("\n", $newComment)));
+		}
+
+		// preg_match_all('#@(?:Middleware|Get|Post|Any)\(.+#', $phpdoc, $matches);
+		// foreach ($matches[0] as $annotation) {
+		// 	$this->addAnnotation($node, $annotation);
+		// }
+
+		return $node;
+	}
+
+	protected function addAnnotation(Node $node, string $annotation) : bool {
+		$annotation = trim($annotation, '@');
+		if (preg_match('#=\s*\{#', $annotation) || str_contains($annotation, '({')) {
+			echo "  WARNING: " . $node->name->name . " contains dangerous {} properties\n";
+			// return false;
+		}
+
+		$attrName = explode('(', $annotation)[0];
+		$namespace = in_array($attrName, self::MODEL_ANNOTATIONS) ? self::MODEL_NAMESPACE : self::ROUTE_NAMESPACE;
+		$annotation = preg_replace('#, (\w+)=#', ', $1: ', $annotation);
+		$attrClass = sprintf('%s\%s', $namespace, $annotation);
+		$node->attrGroups[] = new AttributeGroup([new Attribute(new FullyQualified($attrClass))]);
+		return true;
+	}
+
+}
